ff8e99fe1691c807ad9e395068409b07ca4bc198
fahadalshoaibi <tut72113@temple.edu>
2024-12-01 06:49:36 -0500

    .

--- backend/src/products/Product.controller.js
+++ backend/src/products/Product.controller.js
@@ -14,7 +14,7 @@ const postProduct = async (req, res) => {
 const getProducts = async (req, res) => {
   try {
     const products = await Product.find();
-    res.status(200).send( products );
+    res.status(200).json( products );
   } catch (err) {
     console.error("error", err);
     res.status(500).json({ message: "Internal server error" });
@@ -42,7 +42,7 @@ const getProduct = async (req, res) => {
         if (!product) {
           return res.status(404).json({ message: "Product not found" });
         }
-        res.status(200).send(product);
+        res.status(200).json(product);
 
     }
     catch(err){
